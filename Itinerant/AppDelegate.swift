1f9f6ee173ea290f9023b4bc7cac8d336712087f
Lauren N. Roth <laurennicoleroth@gmail.com>
2017-09-18 14:58:09 -0400

    Make navbar nice app-wide

--- Itinerant/AppDelegate.swift
+++ Itinerant/AppDelegate.swift
@@ -16,7 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    
+    UINavigationBar.appearance().setBackgroundImage(UIImage(named: "navigation")!.resizableImage(withCapInsets: UIEdgeInsetsMake(0, 0, 0, 0), resizingMode: .stretch), for: .default)
+    UINavigationBar.appearance().isTranslucent = false
+    
     return true
   }
 
