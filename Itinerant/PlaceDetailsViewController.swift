c05e8887614a6f5741e4bbd7bc2ef6e9443e2d5d
Lauren N. Roth <laurennicoleroth@gmail.com>
2017-09-18 23:25:58 -0400

    Add marker to map

--- Itinerant/PlaceDetailsViewController.swift
+++ Itinerant/PlaceDetailsViewController.swift
@@ -17,6 +17,8 @@ class PlaceDetailsViewController: UIViewController, GMSMapViewDelegate{
   
   @IBOutlet var addressLabel: UILabel!
   
+  @IBOutlet var mapView: GMSMapView!
+  
   override func viewDidLoad() {
     
     super.viewDidLoad()
@@ -24,6 +26,13 @@ class PlaceDetailsViewController: UIViewController, GMSMapViewDelegate{
     placeNameLabel.text = place?.name
     addressLabel.text = place?.address
     
+    let center = CLLocationCoordinate2D(latitude: (place?.latitude)!, longitude: (place?.longitude)!)
+    
+    let camera = GMSCameraPosition.camera(withLatitude: center.latitude, longitude: center.longitude, zoom: 14, bearing: 30, viewingAngle: 45)
+    mapView.camera = camera
+    
+    let marker = place?.marker
+    marker?.map = mapView
     
   }
   
